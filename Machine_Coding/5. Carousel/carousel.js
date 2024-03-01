607b541e16f166cc9db62e33015ef227fe4d3a38
AbhaySingh5349 <abhay5349singh@gmail.com>
2024-03-01 20:48:32 +0530

    basic image slider

---
+++ Machine_Coding/5. Carousel/carousel.js
@@ -0,0 +1,41 @@
+const slider = document.querySelectorAll('.carousel');
+const nextBtn = document.querySelector('.btn-next');
+const prevBtn = document.querySelector('.btn-prev');
+
+let currentSlide = 0;
+let maxSlide = slider.length - 1;
+
+slider.forEach(
+  (image, index) => (image.style.transform = `translateX(${index * 100}%)`)
+);
+
+function nextButtonClickHandler() {
+  if (currentSlide === maxSlide) {
+    currentSlide = 0; // going back to the first image
+  } else {
+    currentSlide++;
+  }
+
+  // translating current image by -100
+  slider.forEach(
+    (image, index) =>
+      (image.style.transform = `translateX(${(index - currentSlide) * 100}%)`)
+  );
+  console.log(currentSlide);
+}
+
+function prevButtonClickHandler() {
+  if (currentSlide === 0) {
+    currentSlide = maxSlide; // going back to the last image
+  } else {
+    currentSlide--;
+  }
+  slider.forEach(
+    (image, index) =>
+      (image.style.transform = `translateX(${(index - currentSlide) * 100}%)`)
+  );
+  console.log(currentSlide);
+}
+
+nextBtn.addEventListener('click', nextButtonClickHandler);
+prevBtn.addEventListener('click', prevButtonClickHandler);
